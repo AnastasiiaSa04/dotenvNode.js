75ca8faaf4f105e778579c9a8e3491ca4f13635e
Anastasiia <sulollarianastasiia@gmail.com>
2025-10-14 19:27:40 +0200

    Create file with dotenv

---
+++ file-operation.js
@@ -0,0 +1,23 @@
+import fs from "node:fs/promises"
+import dotenv from "dotenv"
+
+dotenv.config({quiet: true});
+
+
+const fileName = process.env.FILENAME;
+    if(!fileName) {
+    console.log("Error: variable FILENAME not found");
+    process.exit(1)
+}
+
+const text = "Hello from node.js"
+try {
+fs.writeFile(fileName, text, "utf-8")
+console.log("Created file")
+
+const info = await fs.readFile(fileName, "utf-8")
+console.log("Files info:")
+console.log(info) 
+} catch (error) {
+    console.log("Error can not fing a file", error)
+}
